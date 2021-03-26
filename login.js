cac7af3a64f29860db090e2e11cd6cdaa1fe49ad
JaeSuYaItHing <yoojw1128@naver.com>
2021-03-26 21:30:40 +0900

    완료 창

--- login.js
+++ login.js
@@ -2,7 +2,8 @@ var id = 'id';
 var password = 'password';
 function login(){
   if(document.querySelector('#id').value === id && document.querySelector('#password').value === password){
-alert('로그인에 성공하셨습니다');  
+alert('로그인에 성공하셨습니다');
+location.href = "complete.html";  
 }else{
   alert('틀림 ㅅㄱ');
   window.location.reload();
